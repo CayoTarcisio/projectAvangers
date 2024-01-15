eae07ae00b2af2be5d16db1256b1576f4338fd82
Cayo Tarcisio <cayotarcisio51@gmail.com>
2024-01-15 01:28:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+  <img src="/assets/logiAvangers.png" width="250px" />
+</p>
+
+<p align="center">
+  <img src="/.github/preview.JPG" width="100%" />
+</p>
+
+## 💻 Projeto
+
+Este projeto é um tutorial que ensina a como criar o site dos **Vingadores** ao usar _HTML_, _CSS_ e _JavaScript_.
+
+## 🚀 Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+
+## 📔 Conhecimentos abordados
+
+- [x] Uso semântico do HTML
+- [x] Uso do CSS Flexbox
+- [x] Carrousel de imagens
+- [x] Menu lateral
+- [x] Carregamento de fonte externa
+- [x] Uso de cores gradientes no texto
+
---
+++ script/index.js
@@ -0,0 +1,52 @@
+const members = [
+  { name: 'Homem de Ferro', id: 'tony' },
+  { name: 'Capitão América', id: 'capitao' },
+  { name: 'O Incrível Hulk', id: 'hulk' },
+  { name: 'Viúva Negra', id: 'viuva' },
+  { name: 'Thor, Deus do Trovao', id: 'thor' },
+  { name: 'Pantera Negra', id: 'pantera' },
+]
+
+let activeMember = 0;
+const images = document.getElementById('images');
+const menu = document.getElementById('menu');
+const navigation = document.getElementById('navigation');
+const memberName = document.getElementById('member__name');
+
+function changeStatusButton() {
+  const prev = document.getElementById('button__prev');
+  const isFirst = activeMember === 0;
+  prev.disabled = isFirst
+
+  const next = document.getElementById('button__next');
+  const isLast = activeMember === members.length - 1;
+  next.disabled = isLast;
+
+}
+
+function changeMember(memberId) {
+  activeMember = memberId
+  const member = members[activeMember];
+
+  console.log(activeMember, member);
+
+  images.style.transform = `translateY(${-100*activeMember}vh)`;
+  memberName.classList = member.id;
+
+  changeName(member.name);
+  changeStatusButton();
+}
+
+function navigationMember(direction) {
+  changeMember(activeMember + direction);
+}
+
+function changeMenu() {
+  menu.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
+
+function setMember(memberId) {
+  changeMember(memberId);
+  changeMenu()
+}
